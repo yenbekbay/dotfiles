855c515e356d079d9a01047b821bfcfe8d552ed6
Ayan Yenbekbay <ayan.yenb@gmail.com>
2016-10-11 16:48:21 +0600

    Support installing apps from MAS with `mas-cli`

--- bootstrap.sh
+++ bootstrap.sh
@@ -103,6 +103,9 @@ gem install xcode-install
 brew tap caskroom/cask
 sudo chflags hidden /opt
 
+# Install mas
+brew install mas
+
 # Install utility apps
 brew cask install appcleaner
 brew cask install flux
@@ -114,6 +117,10 @@ brew cask install qlmarkdown
 brew cask install qlstephen
 brew cask install quitter
 brew cask install suspicious-package
+mas install 437151949 # Entropy
+mas install 937984704 # Amphetamine
+mas install 1082170746 # Aware
+mas install 1000397973 # Wallcat
 
 # Install development apps
 brew cask install docker
@@ -127,12 +134,23 @@ brew cask install postico
 brew cask install postman
 brew cask install sourcetree
 brew cask install visual-studio-code
+brew cask install zeplin
+mas install 412448059 # ForkLift
 
 # Install productivity apps
 brew cask install itsycal
 brew cask install dropbox
 brew cask install rescuetime
 brew cask install shiori
+mas install 692867256 # Simplenote
+mas install 409183694 # Keynote
+mas install 443987910 # 1Password
+mas install 409203825 # Numbers
+mas install 409201541 # Pages
+
+# Install news apps
+mas install 880001334 # Reeder
+mas install 568494494 # Pocket
 
 # Install communication apps
 brew cask install messenger
@@ -140,10 +158,14 @@ brew cask install whatsapp
 brew cask install skype
 brew cask install slack
 brew cask install telegram
+mas install 918858936 # Airmail
+mas install 409789998 # Twitter
 
 # Install media apps
 brew cask install vlc
 brew cask install webtorrent
+mas install 731738567 # Permute
+mas install 597611879 # Radium
 
 # Install other apps
 brew cask install google-photos-backup
