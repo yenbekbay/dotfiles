02f61cd4757cbe68a34a5dcc5b349a818066f6d5
Ayan Yenbekbay <ayan.yenb@gmail.com>
2018-03-21 01:10:57 +0100

    feat(vscode.sh): update settings.json and extensions list

--- vscode.sh
+++ vscode.sh
@@ -1,17 +1,72 @@
 #!/usr/bin/env bash
 
-code --install-extension EditorConfig.EditorConfig
-code --install-extension PeterJausovec.vscode-docker
-code --install-extension WakaTime.vscode-wakatime
-code --install-extension be5invis.toml
-code --install-extension christian-kohler.path-intellisense
 code --install-extension dbaeumer.vscode-eslint
-code --install-extension dkundel.vscode-npm-source
-code --install-extension donjayamanne.githistory
-code --install-extension dzannotti.vscode-babel-coloring
+code --install-extension EditorConfig.EditorConfig
+code --install-extension esbenp.prettier-vscode
 code --install-extension flowtype.flow-for-vscode
-code --install-extension golf1052.theme-base16-ocean
-code --install-extension mrmlnc.vscode-stylefmt
+code --install-extension kumar-harsh.graphql-for-vscode
+code --install-extension PeterJausovec.vscode-docker
 code --install-extension robertohuertasm.vscode-icons
-code --install-extension shinnn.stylelint
 code --install-extension vsmobile.vscode-react-native
+code --install-extension WakaTime.vscode-wakatime
+code --install-extension will-stone.plastic
+
+cat > "~/Library/Application Support/Code/User/settings.json" <<EOF
+{
+  "editor.detectIndentation": true,
+  "editor.fontFamily": "Monoid-Retina, Menlo, monospace",
+  "editor.fontLigatures": true,
+  "editor.fontSize": 11,
+  "editor.fontWeight": "300",
+  "editor.renderWhitespace": "boundary",
+  "editor.rulers": [80],
+  "editor.scrollBeyondLastLine": false,
+  "editor.wordBasedSuggestions": false,
+
+  "extensions.ignoreRecommendations": true,
+
+  "files.associations": {
+    ".flowconfig": "ini",
+    "*.h": "objective-c",
+    "*.js.flow": "javascript",
+    "*.podspec": "ruby",
+    "Appfile": "ruby",
+    "Deliverfile": "ruby",
+    "Fastfile": "ruby",
+    "Gymfile": "ruby",
+    "Matchfile": "ruby",
+    "Podfile": "ruby",
+    "Vagrantfile": "ruby"
+  },
+
+  "flow.runOnEdit": false,
+
+  "gitlens.blame.line.enabled": false,
+  "gitlens.codeLens.enabled": false,
+
+  "javascript.validate.enable": false,
+  "javascript.nameSuggestions": false,
+
+  "search.useRipgrep": false,
+
+  "terminal.integrated.fontSize": 11,
+
+  "window.zoomLevel": 0,
+  "window.openFilesInNewWindow": "off",
+
+  "workbench.colorTheme": "Plastic - deprioritised punctuation",
+  "workbench.iconTheme": "vscode-icons",
+  "workbench.sideBar.location": "right",
+
+  "vsicons.associations.files": [
+    {
+      "icon": "config",
+      "extensions": ["env.example"],
+      "light": true,
+      "format": "FileFormat.svg"
+    }
+  ],
+  "vsicons.dontShowNewVersionMessage": true,
+  "vsicons.projectDetection.disableDetect": true
+}
+EOF
