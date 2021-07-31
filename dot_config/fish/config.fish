2c071d96c6d8e0346a73d9354d68d4d87427a4e9
yenbekbay <ayan.yenb@gmail.com>
2021-07-31 15:51:24 +0600

    Update fish config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,14 +1,17 @@
 # Remove the default greeting
 set -U fish_greeting
 
+# Configure fzf autocomplete (https://github.com/jethrokuan/fzf/wiki/FZF-Tab-Completions#fzf_complete--1-2-3)
+set -U FZF_COMPLETE 2
+
 # Initialize Homebrew (https://github.com/Homebrew/brew)
 eval (/opt/homebrew/bin/brew shellenv)
 
-# Initialize Starship (https://starship.rs/)
-starship init fish | source
-
 # Initialize direnv (https://direnv.net/)
 eval (direnv hook fish)
+
+# Initialize Starship (https://starship.rs/)
+starship init fish | source
 
 # Initialize zoxide (https://github.com/ajeetdsouza/zoxide/)
 zoxide init fish | source
