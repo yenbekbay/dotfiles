b00a4e8e66177bd7cb250670087db0b9da05541d
Ayan Yenbekbay <ayan.yenb@gmail.com>
2023-06-25 16:23:53 +0600

    Update fish config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,31 +1,38 @@
 # Remove the default greeting
 set -U fish_greeting
 
-# Configure fzf autocomplete (https://github.com/jethrokuan/fzf/wiki/FZF-Tab-Completions#fzf_complete--1-2-3)
+# fzf autocomplete (https://github.com/jethrokuan/fzf/wiki/FZF-Tab-Completions#fzf_complete--1-2-3)
 set -U FZF_COMPLETE 2
 
-# Initialize Homebrew (https://github.com/Homebrew/brew)
+# Homebrew (https://github.com/Homebrew/brew)
 eval (/opt/homebrew/bin/brew shellenv)
 
-# Initialize direnv (https://direnv.net/)
+# direnv (https://direnv.net/)
 eval (direnv hook fish)
 
-# Initialize Starship (https://starship.rs/)
+# Starship (https://starship.rs/)
 starship init fish | source
 
-# Initialize zoxide (https://github.com/ajeetdsouza/zoxide/)
+# zoxide (https://github.com/ajeetdsouza/zoxide/)
 zoxide init fish | source
 
-# Initialize asdf (https://asdf-vm.com/)
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
+# asdf (https://asdf-vm.com/)
+source "$(brew --prefix asdf)/libexec/asdf.fish"
 
-# Initialize Google Cloud SDK
-source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+# Google Cloud SDK
+source "$(brew --prefix)/share/google-cloud-sdk/path.fish.inc"
 
-# Add Android Studio environment variables
+# Android Studio
 export ANDROID_HOME=$HOME/Library/Android/sdk
 export ANDROID_SDK_ROOT=$HOME/Library/Android/sdk
 export ANDROID_AVD_HOME=$HOME/.android/avd
 
-# tabtab source for packages
+# tabtab
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
+
+# pnpm
+set -gx PNPM_HOME /Users/yenbekbay/Library/pnpm
+set -gx PATH "$PNPM_HOME" $PATH
+
+# Secretive
+set -x SSH_AUTH_SOCK /Users/yenbekbay/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
