43b84d788036d2a8c214b67150d4209f31f1d2e1
Ayan Yenbekbay <ayan.yenb@gmail.com>
2024-12-29 20:33:10 +0500

    Update fish config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -3,13 +3,19 @@ set -U fish_greeting
 
 # Homebrew
 eval (/opt/homebrew/bin/brew shellenv)
-set -gx HOMEBREW_NO_ENV_HINTS true
+set -x HOMEBREW_NO_ANALYTICS 1
+set -x HOMEBREW_NO_EMOJI 1
+set -x HOMEBREW_NO_ENV_HINTS 1
 
 # Starship
 starship init fish | source
 
 # mise-en-place
-mise activate fish | source
+if status is-interactive
+  mise activate fish | source
+else
+  mise activate fish --shims | source
+end
 
 # direnv
 eval (direnv hook fish)
@@ -20,11 +26,13 @@ set -g sponge_delay 10
 set -g sponge_purge_only_on_exit true
 
 # fzf
+fzf --fish | source
 set -gx FZF_DEFAULT_OPTS --height 80% --reverse
 
 # zoxide
 zoxide init fish | source
-set -gx _ZO_FZF_OPTS "$FZF_DEFAULT_OPTS --keep-right --exit-0 --select-1 --preview='command exa {2..}' --preview-window=bottom"
+set -gx _ZO_FZF_OPTS "$FZF_DEFAULT_OPTS --keep-right --exit-0 --select-1 --preview='command eza {2..}' --preview-window=bottom"
+bind \co 'zi && commandline --function repaint'
 
 # forgit
 set -gx FORGIT_FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS"
@@ -40,10 +48,13 @@ set -x SSH_AUTH_SOCK ~/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/D
 set -gx PNPM_HOME ~/Library/pnpm
 set -gx PATH "$PNPM_HOME" $PATH
 
+# Java
+set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
+
 # Android Studio
 set -gx ANDROID_HOME ~/Library/Android/sdk
-set -gx ANDROID_SDK_ROOT ~/Library/Android/sdk
-set -gx ANDROID_AVD_HOME ~/.android/avd
+set -gx PATH "$ANDROID_HOME/emulator" $PATH
+set -gx PATH "$ANDROID_HOME/platform-tools" $PATH
 
 # Tailscale
 alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
